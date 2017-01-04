3a23766f927bbbede76b9d1667c04adfc66528dd
balivada987 <balivada141@gmail.com>
2017-01-04 22:21:52 +0530

    Commit 2
    After adding all  JSON unable to get markers

--- js/app2.js
+++ js/app2.js
@@ -1,15 +1,39 @@
 /**
- * Created by Shrinivasan on 02-01-2017.
+ * Created by Shrinivasan on 02-11-2016.
  */
 function displaymap() {
   console.log('hi');
+var initialmarkers = [];
+  initialmarkers = [
+
+    {
+      title: 'Riverbanks Zoo and Garden ',
+      address: '500 Wildlife Pkwy, Columbia, SC 29210',
+      description: 'Riverbanks Zoo and Garden ',
+      url: 'http://hollywoodtheatre.org/',
+      latitude: 34.010111,
+      longitude: -81.074432,
+
+    },
+    {
+      title: 'Myrtle Beach Resort',
+      address: '126 SW 2nd Ave, Portland, OR 97204',
+      description: 'Food hall including Salt & Straw\'s whiz bang soft serve bar',
+      url: '5905 S Kings Hwy, Myrtle Beach, SC 29575,',
+      latitude: 33.633161,
+      longitude: -78.95683,
+
+    }
+
+  ];
   var Markerdetails=function (data) {
 
-      this.title = 'Caesars Head State Park Visitor Center';
-    this.latitude = 35.105859;
-    this.longitude =-82.626111;
+      this.title = data.title;
+    this.latitude = Number(data.latitude);
+    this.longitude =Number(data.longitude);
+    alert('this is marker'+this.latitude+','+this.longitude)
     this.latLng ={lat:this.latitude, lng: this.longitude};
-    this.address='8155 Geer Hwy, Cleveland, SC 29635'
+    this.address=data.address
 
 
       var map = new google.maps.Map(document.getElementById('map'), {
@@ -18,7 +42,8 @@ function displaymap() {
 
 
     });
-    alert(this.latitude);
+    alert('this is marker2'+this.latLng.lat+','+this.latLng.lng);
+
      this.marker = new google.maps.Marker({
         position: this.latLng,
 
@@ -26,7 +51,7 @@ function displaymap() {
         title: this.title
       });
 
-    alert('finish');
+    alert(this.latitude+',' +this.longitude);
 
 
 
@@ -36,16 +61,17 @@ function displaymap() {
     var $body = $('body');
     var streetStr = '1000 Whaley Street ';
     var cityStr = 'Columbia';
-    var address = streetStr + ', ' + cityStr;
-    alert(address);
+    var address = this.address;
+    alert(this.address);
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     jsondata.append('<li><img class="" src="' + streetviewUrl + '"></li>');
     alert(jsondata);
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
   'api-key': "c347d6bc6a5b4be1ac897ada807b0293",
-  'q':""+cityStr+"",'sort':'newest'
+  'q':""+this.title+"",'sort':'newest'
   });
+    /*
     $.ajax({
       url: url,
       method: 'GET',
@@ -59,15 +85,18 @@ function displaymap() {
         }
       },
       error: function () {
-        alert('not possible');
+        alert('No article available for'+this.title);
       }
     })
 
 
 
       alert(jsondata);
+
     alert('wiki data');
-  var wikiurl= 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr+ '&format=json&callback=wikiCallback';
+  var wikiurl= 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' +this.title+ '&format=json&callback=wikiCallback';
+
+
    alert(wikiurl);
   $.ajax({
     url: wikiurl,
@@ -95,15 +124,29 @@ var contentstring="<html><div>"+this.title + this.address+"<div id='#jsondata'><
 
      })
     alert(contentstring);
-    /*
+
     this.marker.addListener('click', function() {
-    infowindow.open(map, marker);
+      alert(contentstring);
+    infowindow.open(map, this.marker);
   });
-  */
+
   }
 
   var ViewModel=function(){
-    this.currentmarker=ko.observable(new Markerdetails());
+    var self=this;
+    this.markerlist=ko.observableArray([]);
+    this.markertitles=ko.observableArray([]);
+    initialmarkers.forEach(function (markeritem) {
+      self.markerlist.push(new Markerdetails(markeritem));
+
+      self.markertitles.push(Markerdetails(markeritem.title));
+     // alert('these are titles'+self.markertitles);
+
+
+    })
+    alert(this.markerlist()[0].title);
+    this.currentmarker=ko.observable(this.markerlist()[0]);
+    alert(this.markerlist()[0])
   }
 
 
