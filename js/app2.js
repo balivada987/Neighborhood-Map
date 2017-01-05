cfe49f6bcb96259f9ad5b0881d1b39185a407975
balivada987 <balivada141@gmail.com>
2017-01-05 23:01:43 +0530

    5th Commit
    Search functionality need to implement, Infowindows need to be at the
    right place. Bootstrap has to added

--- js/app2.js
+++ js/app2.js
@@ -3,14 +3,28 @@
  */
 function displaymap() {
   console.log('hi');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+    var $images= $('#images');
+     var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      center: {lat: 34.010111, lng: -81.074432}
+
+
+
+    });
 var initialmarkers = [];
   initialmarkers = [
 
     {
       title: 'Riverbanks Zoo and Garden ',
       address: '500 Wildlife Pkwy, Columbia, SC 29210',
+        cityStr:'Columbia',
       description: 'Riverbanks Zoo and Garden ',
       url: 'http://hollywoodtheatre.org/',
+
       latitude: 34.010111,
       longitude: -81.074432,
 
@@ -20,10 +34,12 @@ var initialmarkers = [];
       address: '126 SW 2nd Ave, Portland, OR 97204',
       description: 'Food hall including Salt & Straw\'s whiz bang soft serve bar',
       url: '5905 S Kings Hwy, Myrtle Beach, SC 29575,',
+        cityStr:'Myrtle Beach',
       latitude: 33.633161,
       longitude: -78.95683,
 
     }
+
 
   ];
   var Markerdetails=function (data) {
@@ -31,18 +47,16 @@ var initialmarkers = [];
       this.title = data.title;
     this.latitude = Number(data.latitude);
     this.longitude =Number(data.longitude);
-    alert('this is marker'+this.latitude+','+this.longitude)
     this.latLng ={lat:this.latitude, lng: this.longitude};
-    this.address=data.address
+    this.address=data.address;
+
+      this.cityStr=data.cityStr;
+      alert('data is'+data.cityStr)
+      alert(this.cityStr);
 
 
-      var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
-      center: {lat: 34.010111, lng: -81.074432}
 
 
-    });
-    alert('this is marker2'+this.latLng.lat+','+this.latLng.lng);
 
      this.marker = new google.maps.Marker({
         position: this.latLng,
@@ -51,37 +65,37 @@ var initialmarkers = [];
         title: this.title
       });
 
-    alert(this.latitude+',' +this.longitude);
 
-
-
-     jsondata=$('#jsondata');
-    $(jsondata).text("<h2>The json data of"+this.title+"is </h2>");
-    alert(jsondata);
     var $body = $('body');
     var streetStr = '1000 Whaley Street ';
-    var cityStr = 'Columbia';
+    var cityStr = this.cityStr;
+      alert(cityStr);
+
     var address = this.address;
-    alert(this.address);
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    jsondata.append('<li><img class="" src="' + streetviewUrl + '"></li>');
-    alert(jsondata);
+
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+cityStr+'';
+      alert('this is street view url'+streetviewUrl);
+
+      $images.append('<img  src="' + streetviewUrl + '">');
+
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
   'api-key': "c347d6bc6a5b4be1ac897ada807b0293",
-  'q':""+this.title+"",'sort':'newest'
+  'q':""+cityStr+"",'sort':'newest'
   });
-    
+     $($nytHeaderElem).text('In'+this.address+'following is going on');
     $.ajax({
       url: url,
       method: 'GET',
       success: function (result) {
         var articles = result.response.docs;
-        alert(result);
+          alert('nyt'+articles[0].web_url);
 
         for (i = 0; i < articles.length; i++) {
 
-          $(jsondata).append('<li><a>' + articles[i].snippet + '</a></li>');
+         $($nytHeaderElem).append('<li><a href=""+articles[i].web_url+""></a></li>');
+            $($nytHeaderElem).append('<li><a>'+articles[i].snippet+'</a></li>');
+
         }
       },
       error: function () {
@@ -91,31 +105,30 @@ var initialmarkers = [];
 
 
 
-      alert(jsondata);
+    var title=this.title;
+      alert(title);
 
     alert('wiki data');
-  var wikiurl= 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' +this.title+ '&format=json&callback=wikiCallback';
-
-
-   alert(wikiurl);
+  var wikiurl= 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' +title+ '&format=json&callback=wikiCallback';
+      alert(wikiurl);
   $.ajax({
     url: wikiurl,
 
     dataType: 'jsonp',
     success: function (result) {
       alert(result);
-      $(jsondata).append('<p>' + result + '</p>');
-      alert(jsondata);
-      this.jsondata=ko.observable(jsondata);
+      $wikiElem.append('<p>'+result+'</p>');
+
+
     },
     error: function () {
       alert('no wiki articles available');
 
     }
   })
-     alert('this is'+jsondata);
 
-var contentstring="<html><div>"+this.title + this.address+"<div id='#jsondata'></div></div></html>";
+
+var contentstring="<html><div>"+this.title + this.address;
     this.contentstring=ko.observable(contentstring);
 
     var infowindow =new google.maps.InfoWindow({
@@ -126,8 +139,17 @@ var contentstring="<html><div>"+this.title + this.address+"<div id='#jsondata'><
     alert(contentstring);
 
     this.marker.addListener('click', function() {
-      alert(contentstring);
-    infowindow.open(map, this.marker);
+         if (infowindow) {
+             for(i=0;i<initialmarkers.length;i++)
+                 infowindow.close();
+    }
+
+        if (infowindow.content===undefined){
+            contentstring="<html><div>"+this.title + this.address+"</div></html>";
+            infowindow.setcontent(contentstring);
+            return infowindow.open(map, this.marker)
+        }
+          else {return infowindow.open(map, this.marker)}
   });
 
   }
@@ -135,19 +157,26 @@ var contentstring="<html><div>"+this.title + this.address+"<div id='#jsondata'><
   var ViewModel=function(){
     var self=this;
     this.markerlist=ko.observableArray([]);
-    this.markertitles=ko.observableArray([]);
+    this.markertitles=[];
     initialmarkers.forEach(function (markeritem) {
       self.markerlist.push(new Markerdetails(markeritem));
 
+
       self.markertitles.push(Markerdetails(markeritem.title));
-     // alert('these are titles'+self.markertitles);
+
+     console.log('these are titles'+self.markertitles);
 
 
     })
     alert(this.markerlist()[0].title);
     this.currentmarker=ko.observable(this.markerlist()[0]);
     alert(this.markerlist()[0])
+      search:function (value) {ViewModel.
+          
+      }
   }
+
+
 
 
    ko.applyBindings(new ViewModel());
