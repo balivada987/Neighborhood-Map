821e77b9ffb64a587e5650560b1c112e11d0a03d
balivada987 <balivada141@gmail.com>
2017-01-18 12:14:48 +0530

    Commit 8
    Only Map is displayed  Error:ERR_QUIC_PROTOCOL_ERROR

--- js/app2.js
+++ js/app2.js
@@ -1,21 +1,27 @@
 /**
  * Created by Shrinivasan on 02-11-2016.
  */
+var map;
+var markerlist=[];
 function displaymap() {
-  console.log('hi');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
-    var $images= $('#images');
-     var map = new google.maps.Map(document.getElementById('map'), {
+  console.log('map');
+
+     map = new google.maps.Map(document.getElementById('map'), {
       zoom: 7,
       center: {lat: 34.010111, lng: -81.074432}
+     });
+    alert('map finished')
+
+ko.applyBindings(new Markerdetails());}
+var mapsError = function() {
+  alert("Google Maps failed to load");
+}
 
 
 
-    });
-var initialmarkers = [];
+  var Markerdetails=function () {
+      alert('Markerdetails started')
+        var initialmarkers = [];
   initialmarkers = [
 
     {
@@ -37,13 +43,57 @@ var initialmarkers = [];
         cityStr:'Myrtle Beach',
       latitude: 33.633161,
       longitude: -78.95683,
-
     }
-
-
   ];
-  var Markerdetails=function (data) {
+      var infoWindow = new google.maps.InfoWindow();
+      alert('infoWindow created')
+      self=this;
+       self.wikiElem = ko.observable('');
+    self.nytArt= ko.observableArray('');
+      self.nyArtlist=ko.observableArray('');
+    self.locations = ko.observableArray(initialmarkers);
+    self.markerSelected=ko.observableArray();
+    self.images= ko.observable('');
 
+      self.userSearch = ko.observable(' ');
+      self.writeToConsole = ko.computed(function() {
+    console.log(self.userSearch());
+  });
+      alert('all declarations done');
+       self.populateMap1 = ko.computed(function() {
+    var searchQuery = self.userSearch().toLowerCase();
+    var filteredPlaces = [];
+
+    if (!searchQuery) {
+      return  allmarkers();
+    } else {
+      console.log("attempt to repopulate map");
+      removeallarkers();
+      filteredmarkers(searchQuery);
+    }
+  });
+      self.selectPlaces1 = ko.computed(function() {
+    var searchQuery = self.userSearch().toLowerCase();
+          alert(searchQuery);
+
+    if (!searchQuery) {
+      return self.locations();
+    } else {
+      console.log("selectPlaces reached else");
+      self.markerSelected([]);
+      self.locations().forEach(function(location) {
+          var title = location.title.toLowerCase();
+
+          if (title.indexOf(searchQuery) !== -1) {
+            self.markerSelected.push(location);
+          }
+      })
+      return self.markerSelected();
+    }
+  });
+
+
+/*
       this.title = data.title;
     this.latitude = Number(data.latitude);
     this.longitude =Number(data.longitude);
@@ -54,37 +104,66 @@ var initialmarkers = [];
       alert('data is'+data.cityStr)
       alert(this.cityStr);
 
+*/
 
 
+function allmarkers() {
+    self.locations().forEach(function (location) {
+       var position=location.latLng;
+        var title=location.title;
+        var cityStr=location.cityStr;
+        // Marker Creation is done here
+         var marker = new google.maps.Marker({
+             map: map,
+             title: title,
+             position: position
+         });
+        // For Search Add markers to array
+
+        markerlist.push(location);
+        // Add infowindow
+        marker.addListener('click',function (){
+            alert('infowindowdetails and getNyarticles and google street view')
+            getnyarticles(title)
+            googlestreetview(cityStr);
+            getwikiarticle(title);
+            populateInfowindow(infoWindow,marker);
 
 
-     this.marker = new google.maps.Marker({
-        position: this.latLng,
+        })
+        location.marker=marker
 
-        map: map,
-        title: this.title
-      });
+    })
+
+}
 
 
-    var $body = $('body');
-    var streetStr = '1000 Whaley Street ';
-    var cityStr = this.cityStr;
+/*function googlestreetview(cityStr) {
+     var $body = $('body');
+
+    var cityStr = cityStr;
       alert(cityStr);
 
-    var address = this.address;
 
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+cityStr+'';
       alert('this is street view url'+streetviewUrl);
 
       $images.append('<img  src="' + streetviewUrl + '">');
 
+
+}
+*/
+    function getnyarticles(title) {
+
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
   'api-key': "c347d6bc6a5b4be1ac897ada807b0293",
-  'q':""+cityStr+"",'sort':'newest'
+  'q':""+title+"",'sort':'newest'
   });
+
+
       alert(cityStr)
-     $($nytHeaderElem).text('In'+this.address+'following is going on');
+     //$($nytHeaderElem).text('In'+title+'following is going on');
     $.ajax({
       url: url,
       method: 'GET',
@@ -94,9 +173,7 @@ var initialmarkers = [];
 
         for (i = 0; i < articles.length; i++) {
 
-         $($nytHeaderElem).append('<li><a href=""+articles[i].web_url+""></a></li>');
-            $($nytHeaderElem).append('<li><a>'+articles[i].snippet+'</a></li>');
-
+        self.nytArt.push('<a href="'+article.web_url+'">'+article.headline.main+'</a>');
         }
       },
       error: function () {
@@ -104,81 +181,105 @@ var initialmarkers = [];
       }
     })
 
-
-
-    var title=this.title;
-      alert(title);
-
-    alert('wiki data');
-  var wikiurl= 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' +title+ '&format=json&callback=wikiCallback';
-      alert(wikiurl);
-  $.ajax({
-    url: wikiurl,
-
-    dataType: 'jsonp',
-    success: function (result) {
-      alert(result);
-      $wikiElem.append('<p>'+result+'</p>');
-
-
-    },
-    error: function () {
-      alert('no wiki articles available');
-
     }
-  })
+function getwikiarticle(title) {
+    alert(title);
+    var wikiurl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + title + '&format=json&callback=wikiCallback';
+    alert(wikiurl);
+    $.ajax({
+        url: wikiurl,
+
+        dataType: 'jsonp',
+        success: function (result) {
+            alert(result);
+            $wikiElem.append('<p>' + result + '</p>');
 
 
-var contentstring="<html><div>"+this.title + this.address;
+        },
+        error: function () {
+            alert('no wiki articles available');
 
-    this.contentstring=ko.observable(contentstring);
-
-    var infowindow =new google.maps.InfoWindow({
-        map:map,
-       content:contentstring
-
-     })
-    alert(contentstring);
-
-    this.marker.addListener('click', function() {
-         if (infowindow) {
-             for(i=0;i<initialmarkers.length;i++)
-                 infowindow.close();
-    }
-
-        if (infowindow.content===undefined){
-            contentstring="<html><div>"+this.title + this.address+"</div></html>";
-            infowindow.setcontent(contentstring);
-            return infowindow.open(map, this.marker)
         }
-          else {return infowindow.open(map, this.marker)}
-  });
+    })
+}
+function populateInfowindow(infowindow,marker) {
 
-  }
+if(infowindow.marker != marker){
+    infowindow.marker=marker
+    var contentstring = "<html><div>" + marker.title+"</div></html>";
+  infowindow.setcontent(contentstring);
+    alert(contentstring);
+    infowindow.open(map,marker)
+    marker.addListener('closeclick', function () {
+        infowindow.marker=null;
 
-  var ViewModel=function() {
-      query: ko.observable(' ');
-      var self = this;
+    });
+}
 
-      this.markerlist = ko.observableArray([]);
-      this.markertitles = [];
-
-      initialmarkers.forEach(function (markeritem) {
-          self.markerlist.push(new Markerdetails(markeritem));
-      })
-  }
-
-          ViewModel.markeritem = ko.dependentObservable(function () {
+}
+function filteredmarkers(userquery) {
+    self.locations().forEach(function (location) {
 
 
-              var search = ViewModel.query.toLowerCase();
-              alert(search);
-              return ko.utils.arrayFilter(initialmarkers, function (markeritem) {
-                  return markeritem.title.toLowerCase().indexOf(search) >= 0;
-              });
-          }, ViewModel);
-     ko.applyBindings(new ViewModel());
-      }
+        var lowertitle= location.title.toLowerCase()
+        if (lowertitle.indexOf(userquery)> -1){
+            alert('searched result found');
+                 var position=location.latLng;
+        var title=location.title;
+        var cityStr=location.cityStr;
+        // Marker Creation is done here
+         var marker = new google.maps.Marker({
+             map: map,
+             title: title,
+             position: position
+         });
+        // For Search Add markers to array
+
+        markerlist.push(location);
+        // Add infowindow
+        marker.addListener('click',function (){
+            alert('infowindowdetails and getNyarticles and google street view')
+            getnyarticles(title)
+            googlestreetview(cityStr);
+            getwikiarticle(title);
+            populateInfowindow(infoWindow,marker);
+
+
+        })
+        location.marker=marker
+
+
+        }
+
+    })
+
+}
+function onclickoffilteredmarker(filteredmarkerdata) {
+    getnyarticles(filteredmarkerdata.title)
+            googlestreetview(filteredmarkerdata.cityStr);
+            getwikiarticle(filteredmarkerdata.title);
+            populateInfowindow(infoWindow,filteredmarkerdata);
+
+}
+function SetAllMarkersOnMap(map) {
+    for(i=0;i<markerlist.length;i++)
+        markerlist[i].setMap(map)
+
+}
+function removeallarkers() {
+    SetAllMarkersOnMap(null);
+}
+self.nyArtlist=ko.computed(function nyarticlesforselected() {
+    return self.nytArt()
+
+});
+
+
+
+}
+
+
+
 
 
 
